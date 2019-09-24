713f541663f576928ed8834034dfa3f0b4d34ac6
Liz Victoria Franco <lizvictoria754@gmail.com>
2019-09-24 08:30:54 -0500

    Agregando funcion mostrar_pelicula

--- pelicula/pelicula.py
+++ pelicula/pelicula.py
@@ -2,7 +2,8 @@ class Pelicula():
     def __init__(self, codigo, nombre):
         self.codigo = codigo
         self.nombre = nombre
+       
 
     def mostrar_pelicula(self):
-        print('Codigo %s' %(self.codigo))
-        print('Nombre %s' %(self.nombre))
+        print('Codigo: %s' %(self.codigo))
+        print('Nombre: %s' %(self.nombre))
