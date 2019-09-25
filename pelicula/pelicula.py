2a1aefe556c231d0c9c60f37829ba28ae99e5119
Liz Victoria Franco <lizvictoria754@gmail.com>
2019-09-24 20:21:13 -0500

    Archivos de test eliminados

--- pelicula/pelicula.py
+++
@@ -1,9 +0,0 @@
-class Pelicula():
-    def __init__(self, codigo, nombre):
-        self.codigo = codigo
-        self.nombre = nombre
-       
-
-    def mostrar_pelicula(self):
-        print('Codigo: %s' %(self.codigo))
-        print('Nombre: %s' %(self.nombre))
